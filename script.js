b05ed509f19536fd7190e559bc42fdf798afbee1
Lara Domingos <lara.domingoss71@gmail.com>
2026-05-25 03:44:59 -0300

    sétimo commit do HTML e primeiro cmmit do JS

---
+++ script.js
@@ -0,0 +1,26 @@
+let conta = 0
+let pessoas = 0
+
+const contaInput = document.querySelector("#conta")
+contaInput.addEventListener("input", receberValorConta)
+
+function receberValorConta(evento) {
+    conta = Number(evento.target.value)
+}
+
+const pessoaInput = document.querySelector("#pessoas")
+pessoasInput.addEventListener("input", receberQuantidadePessoas)
+
+function receberQuantidadePessoas(evento) {
+    const paragrafoErro = document.querySelector(".pessoa #erro")
+    const divErro = document.querySelector (".pessoas .input-box")
+
+    if(evento.target.value === "0"){
+        paragrafoErro.computedStyleMap.display = "block"
+        divErro.setAttribute("id","erro-div")
+    } else {
+        paragrafoErro.computedStyleMap.display = "none"
+        divErro.setAttribute("id", "")
+        pessoas = Number(evento.target.value)
+    }
+}
